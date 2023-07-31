7bb84b69c487658d7020075fea645a67f9eac868
Vinna Pratiwi <37055998+Vinna28@users.noreply.github.com>
2023-07-31 21:35:55 +0700

    Branch vinna - User Login (#9)
    * manual test cases for user login
    
    * menambahkan cypress script untuk testcase user login

--- Cypress/cypress/support/PageObject/Navigation.js
+++ Cypress/cypress/support/PageObject/Navigation.js
@@ -5,11 +5,39 @@ class Navigation {
     }
 
     visitAccount() {
-        cy.visit('https://magento.softwaretestingboard.com//customer/account/');
+        cy.visit('https://magento.softwaretestingboard.com/customer/account/');
     }
 
     visitProduct() {
         cy.visit('https://magento.softwaretestingboard.com/hero-hoodie.html');
+    }
+
+    visitOrderHistory() {
+        cy.visit('https://magento.softwaretestingboard.com/sales/order/history/');
+    }
+
+    visitDownloadProduct() {
+        cy.visit('https://magento.softwaretestingboard.com/downloadable/customer/products/');
+    }
+
+    visitWishList() {
+        cy.visit('https://magento.softwaretestingboard.com/wishlist/');
+    }
+
+    visitAddressBook() {
+        cy.visit('https://magento.softwaretestingboard.com/customer/address/');
+    }
+
+    visitAccountInformation() {
+        cy.visit('https://magento.softwaretestingboard.com/customer/account/edit/');
+    }
+
+    visitPaymentMethod() {
+        cy.visit('https://magento.softwaretestingboard.com/vault/cards/listaction/');
+    }
+
+    visitProductReview() {
+        cy.visit('https://magento.softwaretestingboard.com/review/customer/');
     }
 }
 
